7a296d7b322bddf5d1e07ac3eb2ed72ec8601a4f
Regan Tan <regantanyk@gmailcom>
2023-07-09 21:45:35 +0800

    improvements to main.py with realtime

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App 
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import time
 
 from filesharer import FileSharer
 
@@ -10,13 +11,17 @@ class CameraScreen(Screen):
     def start(self):
         self.ids.camera.play = True
         self.ids.camera_button.text = 'Stop Camera'
+        self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
         self.ids.camera.play = False
         self.ids.camera_button.text = 'Start Camera'
+        self.ids.camera.texture = None
 
     def capture(self):
-        pass
+        current_time = time.strftime('%Y%m%D-%H%M%S')
+        filename = current_time + ".png"
+        self.ids.camera.export_to_png(filename)
 
 class ImageScreen(Screen):
     pass
@@ -30,6 +35,6 @@ class MainApp(App):
         return RootWidget()
     
 
-
+ 
 
 MainApp().run()
